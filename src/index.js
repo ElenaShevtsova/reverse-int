536398487777fdfa561bd8b70f6b29ec156fcd6b
Elena <helena_shevts@mail.ru>
2020-02-22 12:04:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let array = n.toString().split('');
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== "-") {
+            newArr.push(array[i]);
+        }
+    }
+    let result = newArr.reverse().join('');
+    return Number(result);
 }
